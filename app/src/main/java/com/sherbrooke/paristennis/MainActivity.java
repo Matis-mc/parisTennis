76eda104e1e9098715f00cdc6f9123a9de9d4500
tristan <tristanguignard86@gmail.com>
2022-09-26 23:39:03 -0400

    Mise a jour graphique

--- app/src/main/java/com/sherbrooke/paristennis/MainActivity.java
+++ app/src/main/java/com/sherbrooke/paristennis/MainActivity.java
@@ -1,40 +1,47 @@
 package com.sherbrooke.paristennis;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sherbrooke.paristennis.service.MatchService;
+import com.sherbrooke.paristennis.service.MyRecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-
-    public void init(){
-        button = findViewById(R.id.start);
-        startServiceOnClick();
-    }
-
-    public void startServiceOnClick(){
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MatchService.class);
-                Log.e("MainActivity","start service");
-                startService(intent);
-            }
-        });
-    }
-
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvParis);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener((MyRecyclerViewAdapter.ItemClickListener) this);
+        recyclerView.setAdapter(adapter);
+        
+    }
+
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
